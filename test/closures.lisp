0d28b5d7d2df9cd10974b80a342f76fb84a7357f
Jaraš Lojka <jaras.lojka@nordcloud.com>
2022-10-05 10:39:25 +0200

    Add tests for closures

---
+++ test/closures.lisp
@@ -0,0 +1,22 @@
+(defun outer (a)
+  (lambda () (list a)))
+(setq fn (outer 2))
+(print (fn))
+
+(setq fn (let ((a 5)
+               (b 6))
+           (let ((a 'override))
+             (lambda () (list a b)))))
+(print (fn))
+
+(setq fn (let (lexical)
+           (lambda () (list (boundp 'lexical) (boundp 'dynamic)))))
+(let (dynamic)
+  (print (fn)))
+
+(print (boundp 'outer))
+(print (boundp 'fn))
+(print (boundp 'lexical))
+(print (boundp 'dynamic))
+(print (boundp 'a))
+(print (boundp 'b))
