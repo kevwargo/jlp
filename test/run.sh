378e4c5fbbdf8bb66aaa9662267b90d7e8630b47
Jaraš Lojka <jaras.lojka@nordcloud.com>
2022-10-19 15:03:33 +0200

    Add jar-with-dependencies plugin

--- test/run.sh
+++ test/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-CLASSPATH=$(realpath $(dirname $0)/../target/classes)
+cd $(dirname $(dirname $0))
 
-cd $(dirname $0)
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 exitcode=0
-for t in $(ls | grep -oP '.+(?=\.lisp$)'); do
-    if diff -u ${t}.log <(java -cp $CLASSPATH kevwargo.jlp.Main ${t}.lisp | sed -e '$a\'); then
+for t in $(ls test | grep -oP '.+(?=\.lisp$)'); do
+    if diff -u test/${t}.log <(java -jar target/jlp-${VERSION}-jar-with-dependencies.jar test/${t}.lisp | sed -e '$a\'); then
         echo "$t OK"
     else
         exitcode=1
