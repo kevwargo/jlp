fdbd1b01d5377aec5b2ad486f7d4f42ae7331f27
Jaraš Lojka <jaras.lojka@nordcloud.com>
2022-10-20 13:23:24 +0200

    Add option parser stub

--- src/kevwargo/jlp/Main.java
+++ src/kevwargo/jlp/Main.java
@@ -1,10 +1,20 @@
 package kevwargo.jlp;
 
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
 import java.io.FileInputStream;
 
 public class Main {
 
     public static void main(String args[]) throws Exception {
+        CommandLineParser parser = new DefaultParser();
+        Options opts = new Options();
+        opts.addOption(
+                new Option("r", "read", false, "Only read the Lisp expressions, don't eval them"));
+
         LispProcessor proc = LispProcessor.getInstance();
 
         if (args.length > 1) {
