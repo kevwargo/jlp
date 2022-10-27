fd057b7f4d62bb1eef24f4018ad375b3db1ac0f0
Jaraš Lojka <jaras.lojka@nordcloud.com>
2022-10-28 01:18:00 +0200

    Enrich the try-catch-finally test

--- test/try.lisp
+++ test/try.lisp
@@ -5,3 +5,7 @@
 
 (try (java.io.FileInputStream "53fb6eef-02af-4a9c-89ed-1422b3a01a83")
      ((java.io.FileNotFoundException e) (print e)))
+
+(print (try (java.io.FileInputStream "bbcdb361-f64f-4111-b197-640c3411e204")
+            (java.io.FileNotFoundException)
+            (finally (print "finally 2"))))
